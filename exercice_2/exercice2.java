16db559ce2230b9f97847c9cbe14effb6c9fc103
Joyce <fogangbj@3il.fr>
2024-11-26 14:53:04 +0100

    correction de l'exercice 2

--- exercice_2/exercice2.java
+++ exercice_2/exercice2.java
@@ -1,21 +1,52 @@
 public class GestionnaireNotes {
 
-    // Affiche note et moyennes
+    /**
+     * Affiche les notes et la moyenne d'un étudiant
+     * 
+     * @param nomEtudiant Le nom de l'étudiant pour lequel afficher les notes et la moyenne
+     * @param notes Le tableau des notes de l'étudiant
+     */
     public void afficherNotesEtMoyenne(String nomEtudiant, int[] notes) {
-        // Affichage des notes
+        // Vérifier si le tableau de notes est vide pour éviter une division par zéro
+        if (notes == null || notes.length == 0) {
+            System.out.println("Aucune note disponible pour l'étudiant : " + nomEtudiant);
+            return;
+        }
+
+        
+        afficherNotes(nomEtudiant, notes);
+
+        
+        double moyenne = calculerMoyenne(notes);
+
+        
+        System.out.println("Moyenne de " + nomEtudiant + " : " + moyenne);
+    }
+
+    /**
+     * Affiche les notes d'un étudiant
+     * 
+     * @param nomEtudiant Le nom de l'étudiant dont les notes sont affichées
+     * @param notes Le tableau des notes de l'étudiant
+     */
+    private void afficherNotes(String nomEtudiant, int[] notes) {
         System.out.println("Notes de " + nomEtudiant + ":");
         for (int note : notes) {
             System.out.println("- " + note);
         }
+    }
 
-        // Calcul de la somme des notes
+    /**
+     * Calcule la moyenne des notes
+     * 
+     * @param notes Le tableau des notes à utiliser pour le calcul de la moyenne
+     * @return La moyenne des notes sous forme de nombre à virgule flottante
+     */
+    private double calculerMoyenne(int[] notes) {
         int somme = 0;
         for (int note : notes) {
             somme += note;
         }
-
-        // Affichage de la moyenne
-        double moyenne = (double) somme / notes.length;
-        System.out.println("Moyenne : " + moyenne);
+        return (double) somme / notes.length;
     }
 }
